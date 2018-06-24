4f0dbdfe5ecc47b297297e3c3003b7f86934172a
Amos <asidelnik@gmail.com>
2018-06-24 20:27:55 +0300

    MVP components - created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ParentMainComponent } from './parent-main/parent-main.component';
 import { ChildTasksComponent } from './child-tasks/child-tasks.component';
+import { HeaderComponent } from './header/header.component';
+import { ParentChildComponent } from './parent-child/parent-child.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { TaskComponent } from './task/task.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ParentMainComponent,
-    ChildTasksComponent
+    ChildTasksComponent,
+    HeaderComponent,
+    ParentChildComponent,
+    TasksComponent,
+    TaskComponent
   ],
   imports: [
     BrowserModule
