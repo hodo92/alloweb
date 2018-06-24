49d1e48668b62e8006c5b42a2f58330ab8e7f861
Amos <asidelnik@gmail.com>
2018-06-24 20:23:09 +0300

    Child & parent models - edited

--- src/app/models/child.ts
+++ src/app/models/child.ts
@@ -1,12 +1,12 @@
 export class Child {
     user_id: number;
-    private parent_id: number;
+    parent_id: number;
     first_name: string;
     last_name: string;
     email: string;
     user_img: string;
     balance: number;
-    private _is_parent: boolean = false;
+    is_parent: boolean = false;
 }
 
 
--- src/app/models/parent.ts
+++ src/app/models/parent.ts
@@ -1,10 +1,10 @@
 export class Parent {
     user_id: number;
-    private parent_id: number = null;
+    parent_id: number = null;
     first_name: string;
     last_name: string;
     email: string;
     user_img: string;
     balance: number;
-    private is_parent: boolean = true;
+    is_parent: boolean = true;
 }
