6269a6ab1bcd8934a874f2d206e19e2bbd1c42b9
Amos <asidelnik@gmail.com>
2018-06-24 20:21:28 +0300

    child-tasks & parent main components, and child, parent, and task models created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ParentMainComponent } from './parent-main/parent-main.component';
+import { ChildTasksComponent } from './child-tasks/child-tasks.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ParentMainComponent,
+    ChildTasksComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/models/child.ts
@@ -0,0 +1,13 @@
+export class Child {
+    user_id: number;
+    private parent_id: number;
+    first_name: string;
+    last_name: string;
+    email: string;
+    user_img: string;
+    balance: number;
+    private _is_parent: boolean = false;
+}
+
+
+
