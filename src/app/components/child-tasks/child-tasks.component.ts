2fd95c726659f373ce055a37fea764aec3c3c8cc
Amos <asidelnik@gmail.com>
2018-06-26 22:08:55 +0300

    Add task card - created

--- src/app/components/child-tasks/child-tasks.component.ts
+++ src/app/components/child-tasks/child-tasks.component.ts
@@ -1,16 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-child-tasks',
-  templateUrl: './child-tasks.component.html',
-  styleUrls: ['./child-tasks.component.css']
+    selector: 'app-child-tasks',
+    templateUrl: './child-tasks.component.html',
+    styleUrls: ['./child-tasks.component.css']
 })
 export class ChildTasksComponent implements OnInit {
+    inputFocus: boolean = false;
+    constructor() { }
 
-  constructor() { }
+    ngOnInit() {
 
-  ngOnInit() {
-      
-  }
+    }
+
+    showAllAddTask() {
+        console.log("Trying to focus");
+        
+        this.inputFocus = true;
+    }
+
+    addTask() {
+        this.inputFocus = false;
+    }
 
 }
