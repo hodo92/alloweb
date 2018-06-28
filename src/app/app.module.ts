7d948be63b3d2d0300e92c3cb21cb52cc96b3493
hodo92 <hody92@icloud.com>
2018-06-28 20:04:53 +0300

    try to do the multer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -46,7 +46,7 @@ import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatSelectModule } from '@angular/material/select';
 
-
+import { FileUploadModule } from 'ng2-file-upload';
 
 
 @NgModule({
@@ -87,7 +87,9 @@ import { MatSelectModule } from '@angular/material/select';
         MatDatepickerModule,
         MatSelectModule,
         MatNativeDateModule,
-        JsonpModule
+        JsonpModule,
+        FileUploadModule,
+        MatSelectModule 
     ],
     entryComponents: [
             AddChildComponent
