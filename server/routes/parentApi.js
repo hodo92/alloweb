d95d0ce5bd28413df97b180977d8b5fd98e4954d
Amos Sidelnik <asidelnik@gmail.com>
2018-06-28 14:28:28 +0300

    Update parentApi.js

--- server/routes/parentApi.js
+++ server/routes/parentApi.js
@@ -1,43 +1,4 @@
-const express = require('express');
-const router = express.Router();
-const user = require('../dataAccess/user-model');
-const task = require('../dataAccess/task-model')
 
-const Sequelize = require('sequelize');
-const Op = Sequelize.Op;
-
-// get parent by Email when login
-router.get('/:email', async (req, res) => {
-    let pemail = req.params.email
-    try {
-        res.send(JSON.stringify(await user.getParent(pemail)));
-    } catch (err) {
-        alert(err);
-    }
-})
-
-
-// get kids by parent ID
-router.get('/getKidsbyParent/:parentId', async (req, res) => {
-    let parentId = req.params.parentId
-    try {
-        res.send(JSON.stringify(await user.getKids(parentId)));
-    } catch (err) {
-        alert(err);
-    }
-})
-//add a new child
-router.post('/addChild/', async (req, res) => {
-    let newChild = req.body.newChild;
-    let parentID = newChild.parent_id;
-    console.log(newChild);
-    await user.addChild(newChild);
-    res.send(JSON.stringify(await user.getKids(parentID)));
-})
-
-
-module.exports = router;
-/*
 const express = require('express');
 const router  = express.Router();
 const user= require('../dataAccess/user-model');
@@ -87,4 +48,3 @@ router.post('/addChild/', async (req, res) => {
 
 
 module.exports = router;
-*/
