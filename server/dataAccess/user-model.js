9ca12440d33eccc30c3231c2bd5ef2bc0c0673eb
Amos <asidelnik@gmail.com>
2018-06-28 12:22:56 +0300

    Fixed bugs

--- server/dataAccess/user-model.js
+++ server/dataAccess/user-model.js
@@ -3,41 +3,57 @@ const dataAccess = require('./dataAccess');
 
 
 class User {
-    constructor(){
- this.model = dataAccess.connection.define('User', {
-    user_id:{
-        type: Sequelize.INTEGER,
-        primaryKey: true
-    },
-    parent_id:{
-        type: Sequelize.INTEGER,
-    },
-    first_name: {
-        type: Sequelize.STRING(30)
-    },
-    last_name: {
-        type: Sequelize.STRING(30),
-    },
-    email: {
-        type: Sequelize.STRING(30)
-    },
-    user_img: {
-        type: Sequelize.STRING(200)
-    },
-    balance: {
-        type: Sequelize.INTEGER
-    },
-    is_parent: {
-        type: Sequelize.BOOLEAN
-    }
-}) 
-this.model.belongsTo(this.model, {foreignKey: 'parent_id'});
+    constructor() {
+        this.model = dataAccess.connection.define('User', {
+            user_id: {
+                type: Sequelize.INTEGER,
+                primaryKey: true
+            },
+            parent_id: {
+                type: Sequelize.INTEGER,
+            },
+            first_name: {
+                type: Sequelize.STRING(30)
+            },
+            last_name: {
+                type: Sequelize.STRING(30),
+            },
+            email: {
+                type: Sequelize.STRING(30)
+            },
+            user_img: {
+                type: Sequelize.STRING(200)
+            },
+            balance: {
+                type: Sequelize.INTEGER
+            },
+            is_parent: {
+                type: Sequelize.BOOLEAN
+            }
+        })
+        this.model.belongsTo(this.model, { foreignKey: 'parent_id' });
     }
 
-addChild(newChild){
-    return this.model.create(newChild)
+    addChild(newChild) {
+        return this.model.create(newChild)
+    }
+
+    getParent(pemail) {
+        return user.model.findAll({
+            where: {
+                email: pemail
+            }
+        });
+    }
+    getKids(parentId) {
+        return user.model.findAll({
+            where: {
+                parent_id: parentId
+            }
+        });
     }
 }
+
 
 const user = new User();
 module.exports = user;
