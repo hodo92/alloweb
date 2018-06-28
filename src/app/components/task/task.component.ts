499927311a34220de21b9a5df0870564626b76a8
Amos Sidelnik <asidelnik@gmail.com>
2018-06-28 09:48:49 +0300

    Update task.component.ts

--- src/app/components/task/task.component.ts
+++ src/app/components/task/task.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, OnChanges, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Task } from '../../models/task';
 // import { TaskService } from '../../services/task.service';
 import { ParentTasksComponent } from '../parent-tasks/parent-tasks.component';
@@ -9,7 +9,7 @@ import { ParentTasksComponent } from '../parent-tasks/parent-tasks.component';
     templateUrl: './task.component.html',
     styleUrls: ['./task.component.css']
 })
-export class TaskComponent implements OnInit, OnChanges {
+export class TaskComponent implements OnInit {
 
     color = 'accent';
     taskCompletedCheck = false;
