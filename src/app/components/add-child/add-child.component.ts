675699de32f0198756d02e2393c22b8e622d4cad
dsalvat596 <dsalvat596@gmail.com>
2018-07-01 19:33:30 +0300

    fixed no-tasks bug, added default avatar if no upload

--- src/app/components/add-child/add-child.component.ts
+++ src/app/components/add-child/add-child.component.ts
@@ -36,16 +36,22 @@ export class AddChildComponent implements OnInit {
 
 
   ngOnInit() {
-   // this.route.params.subscribe((params) =>{
-      this.newChild.parent_id = this.data.user_id
-      this.newChild.is_parent = false;
-      this.newChild.balance = 0
-      console.log(this.newChild)
-    //})
+    this.route.params.subscribe((params) =>{
+     this.newChild.parent_id = this.data.user_id
+    // this.newChild.is_parent = false;
+    // this.newChild.balance = 0
+    //   console.log(this.newChild)
+    })
   }
 
-  addChild(child: Child){debugger;
-    this.childService.addNewChild(child)
+  addChild(child: Child){
+    if(this.newChild.user_img == null){
+      this.newChild.user_img = 'http://3.bp.blogspot.com/-qDc5kIFIhb8/UoJEpGN9DmI/AAAAAAABl1s/BfP6FcBY1R8/s1600/BlueHead.jpg'
+    }
+    this.newChild.parent_id = this.data.user_id
+    this.newChild.is_parent = false;
+    this.newChild.balance = 0;
+    this.childService.addNewChild(child);
     this.dialogRef.close();
   }
 }
