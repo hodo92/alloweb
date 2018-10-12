42bb6b2f13af62c921725420b557fc1590ce8e92
hodo92 <hody92@icloud.com>
2018-10-12 11:01:58 +0300

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use('/wishList', wishListApi);
 
 
 app.all('*', (req, res) => {
-    res.sendFile(path.join(__dirname,'index.html' ));
+    res.sendFile(path.join(__dirname,'dist/alloweb/index.html' ));
 });
 
 /**
